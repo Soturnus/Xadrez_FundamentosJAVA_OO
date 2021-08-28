c7ee44a9548ccc7472068b7175cfc41cb9e91d03
Soturnus <rivaldo.valhalla@gmail.com>
2021-08-28 11:15:54 -0300

    exceções das posições, e melhoria da posição das peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package chess;
 
 import boardgame.Board;
-import boardgame.Position;
 import chess.pieces.King_Rei;
 import chess.pieces.Rook_Torre;
 
@@ -24,9 +23,13 @@ public class ChessMatch {
 		return mat;
 	}
 	
+	private void placeNewPiece(char column, int row, ChessPiece piece) {
+		board.placePiece(piece, new ChessPosition(column, row).toPosition());
+	}
+	
 	private void initialSetup() {
-		board.placePiece(new Rook_Torre(board, Color.WHITE), new Position(2, 1));
-		board.placePiece(new King_Rei(board, Color.BLACK), new Position(0, 4));
-		board.placePiece(new King_Rei(board, Color.WHITE), new Position(7, 4));
+		placeNewPiece('b', 6, new Rook_Torre(board, Color.WHITE));
+		placeNewPiece('e', 8, new King_Rei(board, Color.BLACK));
+		placeNewPiece('e', 1, new King_Rei(board, Color.WHITE));
 	}
 }
